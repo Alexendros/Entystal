88464cd6d1dba65de65beaa64b0158c28b2feba1
Alexendros <176752386+Alexendros@users.noreply.github.com>
2025-06-29 06:46:06 +0200

    Añade codecs JSON para tipos básicos

---
+++ core/src/main/scala/entystal/model/Investment.scala
@@ -0,0 +1,31 @@
+package entystal.model
+
+trait Investment {
+  def id: String
+  def timestamp: Long
+  def quantity: BigDecimal
+}
+
+final case class BasicInvestment(
+    id: String,
+    quantity: BigDecimal,
+    timestamp: Long
+) extends Investment
+
+final case class EconomicInvestment(
+    id: String,
+    quantity: BigDecimal,
+    timestamp: Long
+) extends Investment
+
+final case class HumanInvestment(
+    id: String,
+    quantity: BigDecimal,
+    timestamp: Long
+) extends Investment
+
+final case class OperationalInvestment(
+    id: String,
+    quantity: BigDecimal,
+    timestamp: Long
+) extends Investment
