590ea04e9c6c57a8522f0cd88540b8c0c554cb7d
Alexendros <176752386+Alexendros@users.noreply.github.com>
2025-07-02 22:09:40 +0200

    test: añadir pruebas de CLI y Balance

---
+++ core/src/test/scala/entystal/model/BalanceSpec.scala
@@ -0,0 +1,21 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import entystal.model._
+
+class BalanceSpec extends AnyFlatSpec with Matchers {
+  "netWorth" should "calcularse correctamente" in {
+    val assets = List(
+      DataAsset("a1", "d1", 1L, BigDecimal(10)),
+      DataAsset("a2", "d2", 2L, BigDecimal(5))
+    )
+    val liabilities = List(
+      BasicLiability("l1", BigDecimal(4), 3L)
+    )
+    val balance = Balance(assets, liabilities)
+    balance.netWorth shouldBe BigDecimal(11)
+  }
+
+  it should "soportar listas vacías" in {
+    Balance(Nil, Nil).netWorth shouldBe BigDecimal(0)
+  }
+}
